5730897b970a9a098638cc7dbc9d4feaa1707116
ibjang <ibjangb@gmail.com>
2017-05-17 14:27:48 +0900

    Add Function -. getSystemName

--- test.c
+++ test.c
@@ -3,4 +3,18 @@
 void main()
 {
   printf("hi my name is inbok jang.\r\n");
+
+  getSystemName();
+}
+
+
+void getSystemName()
+{
+  int i;
+
+  for (i = 0 ; i <= 10 ; i++ )
+  {
+    /* code */
+    printf("c : %d\r\n", i);
+  }
 }
